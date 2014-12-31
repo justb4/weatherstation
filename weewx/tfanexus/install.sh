76c7f5c391fad6e5487c5e3ddd01e10fa21ba793
Just van den Broecke <just@justobjects.nl>
2014-12-31 15:20:36 +0100

    wunderground station add install proc

--- weewx/tfanexus/install.sh
+++ weewx/tfanexus/install.sh
@@ -5,7 +5,7 @@ WEEWX=/opt/weewx/weewxinst
 # /etc/init.d/weewx stop
 
 cp weewx.conf $WEEWX
-# sed -i "s/wupasswd/`cat wupasswd`/g" $WEEWX/weewx.conf
+sed -i "s/wupasswd/`cat wupasswd`/g" $WEEWX/weewx.conf
 rm -rf $WEEWX/skins/byteweather
 cp -r byteweather $WEEWX/skins/byteweather
 scp -r byteweather/* ftpju011@boshut:/home/f/ftpju011/Webhosting/htdocs/boshut.justobjects.nl/weer
