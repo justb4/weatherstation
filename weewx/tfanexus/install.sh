6df1ca1ad21d2b4674b4efb55678b603971f4a53
Just van den Broecke <just@justobjects.nl>
2015-09-17 11:34:06 +0200

    WOW config added to weewx

--- weewx/tfanexus/install.sh
+++ weewx/tfanexus/install.sh
@@ -6,6 +6,7 @@ WEEWX=/opt/weewx/weewxinst
 
 cp weewx.conf $WEEWX
 sed -i "s/wupasswd/`cat wupasswd`/g" $WEEWX/weewx.conf
+sed -i "s/wowpasswd/`cat wowpasswd`/g" $WEEWX/weewx.conf
 rm -rf $WEEWX/skins/byteweather
 cp -r byteweather $WEEWX/skins/byteweather
 scp -r byteweather/* ftpju011@boshut:/home/f/ftpju011/Webhosting/htdocs/boshut.justobjects.nl/weer
